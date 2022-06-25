1d82f8a2ad3944fd60a822a6d0609ec902fa5c3b
kirti-choudhary <108160574+kirti-choudhary@users.noreply.github.com>
2022-06-25 20:57:18 +0530

    Create shopping.h

---
+++ shopping.h
@@ -0,0 +1,96 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Product ,Item ,Cart
+class Item;
+class Cart;
+
+class Product{
+	int id;
+	string name;
+	int price;
+
+public:
+	Product(){}//default constuctor ele it will show error
+	Product(int uq_id, string name,int price){
+		id = uq_id;
+		this->name = name;//assignment 
+		this->price = price;//using this because names are similar
+	}
+	string getDisplayName(){
+		return name + " : Rs " + to_string(price) + " \n";
+	}
+	string getName(){
+		return name;
+	}
+	friend class Item;
+	friend class Cart;
+};
+
+class Item{
+	Product product;
+	int quantity;
+
+public:
+	//Constructor using a Initialisation List
+	Item(){}
+	Item(Product p, int q):product(p), quantity(q){} //initialisation
+
+	int getItemPrice(){
+		return quantity * product.price;
+	}
+	string getItemInfo(){
+		return to_string(quantity) + " x " + product.name + " Rs. " + to_string(quantity * product.price) + "\n";
+	}
+
+	friend class Cart;
+};	
+
+
+class Cart{
+	unordered_map<int,Item>  items;
+
+public:
+
+	void addProduct(Product product){
+		if(items.count(product.id)==0){
+			Item newItem(product,1);
+			items[product.id] = newItem;
+		}
+		else{
+			items[product.id].quantity += 1;
+		}
+	}
+
+	int getTotal(){
+		//Todo
+		int total = 0;
+		for(auto itemPair : items){
+			auto item = itemPair.second;
+			total += item.getItemPrice();
+		}
+		return total;
+
+	}
+
+	string viewCart(){
+		if(items.empty()){
+			return "Sorry! Cart is empty";	
+		}
+
+		string itemizedList;
+		int cart_total = getTotal();
+
+		for(auto itemPair : items){
+			auto item = itemPair.second;
+			itemizedList.append(item.getItemInfo());
+		}
+
+		return itemizedList + "\n Total Amount : Rs. " + to_string(cart_total) + '\n';
+	}
+	//later on
+	bool isEmpty(){
+		return items.empty();
+	}
+
+};
+
